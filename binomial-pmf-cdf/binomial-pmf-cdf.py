fa19809687a56fba8cb749af96293b1b86893039
Nguyễn Võ Anh Duy <duy.nguyen.17042005@gmail.com>
2026-05-01 16:37:41 +0700

    Update solution for binomial-pmf-cdf

--- binomial-pmf-cdf/binomial-pmf-cdf.py
+++ binomial-pmf-cdf/binomial-pmf-cdf.py
@@ -5,10 +5,16 @@ def binomial_pmf_cdf(n, p, k):
     """
     Compute Binomial PMF and CDF.
     """
-    n = int(n)
-    p = float(p)
-    k = int(k)
+    try:
+        n = int(n)
+        p = float(p)
+        k = int(k)
+    except:
+        return None
 
+    if n < 0 or p < 0 or p > 1 or k < 0 or k > n:
+        return None
+    
     pmf, cdf = 0, 0
     
     pmf = comb(n, k)*(p**k)*(1-p)**(n-k)
