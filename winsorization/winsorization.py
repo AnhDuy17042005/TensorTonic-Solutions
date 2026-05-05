c246eb838541c31ad3cd86fc3711a57c9e17c564
Nguyễn Võ Anh Duy <duy.nguyen.17042005@gmail.com>
2026-05-05 20:37:01 +0700

    Update solution for winsorization

--- winsorization/winsorization.py
+++ winsorization/winsorization.py
@@ -8,15 +8,8 @@ def winsorize(values, lower_pct, upper_pct):
         values = np.asarray(values, dtype=int)
     except:
         return None
-    
-    n = len(values)
 
     lower = np.percentile(values, lower_pct)
     upper = np.percentile(values, upper_pct)
 
-    values = np.where(values<lower, lower, values)
-    values = np.where(values>upper, upper, values)
-
-    print(values)
-
-    return values.tolist()
+    return np.clip(values, lower, upper).tolist()
