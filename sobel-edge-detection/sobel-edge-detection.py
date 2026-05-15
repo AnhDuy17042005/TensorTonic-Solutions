757e0607bb8e2f4e2689cbf64055d6272aee95e4
Nguyễn Võ Anh Duy <duy.nguyen.17042005@gmail.com>
2026-05-15 13:47:43 +0700

    Update solution for sobel-edge-detection

--- sobel-edge-detection/sobel-edge-detection.py
+++ sobel-edge-detection/sobel-edge-detection.py
@@ -4,9 +4,9 @@ def sobel_edges(image):
     """
     Apply the Sobel operator to detect edges.
     """
-    img = np.array(image, dtype=np.float32)
-    h, w = img.shape
-    
+    image = np.array(image, dtype=np.float32)
+    h, w = image.shape
+
     Kx = np.array([
         [-1, 0, 1],
         [-2, 0, 2],
@@ -19,16 +19,22 @@ def sobel_edges(image):
         [1, 2, 1]
     ], dtype=np.float32)
 
-    padded = np.pad(img, pad_width=1, mode="constant", constant_values=0)
-    output = np.zeros((h, w), dtype=np.float32)
+    k_size = Kx.shape[0]
+    padded = np.pad(
+        image, 
+        pad_width=1,
+        mode="constant",
+        constant_values=0
+    )
+
+    output = np.zeros((h, w))
 
     for i in range(h):
         for j in range(w):
-            patch = padded[i:i+3, j:j+3]
+            pad = padded[i:i+k_size, j:j+k_size]
 
-            Gx = np.sum(patch * Kx)
-            Gy = np.sum(patch * Ky)
+            gx = np.sum(pad * Kx)
+            gy = np.sum(pad * Ky)
+            output[i][j] = np.sqrt(gx**2 + gy**2)
 
-            output[i][j] = np.sqrt(Gx**2 + Gy**2)
-    
     return output.tolist()
