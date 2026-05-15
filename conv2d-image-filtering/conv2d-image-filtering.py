db412955980d5878be5359fd9778b094b8212024
Nguyễn Võ Anh Duy <duy.nguyen.17042005@gmail.com>
2026-05-15 13:36:53 +0700

    Update solution for conv2d-image-filtering

--- conv2d-image-filtering/conv2d-image-filtering.py
+++ conv2d-image-filtering/conv2d-image-filtering.py
@@ -1,43 +1,42 @@
 import numpy as np
 
-def compute_ouput_dim(H, W, k_h, k_w, p, s):
+def compute_output_dim(H, W, p, s, k_size):
     """
-    Compute ouput dimension 
+    Compute output dimentions
     """
-    H_out = (H + 2*p - k_h) // s + 1
-    W_out = (W + 2*p - k_w) // s + 1
+    H_out = (H + 2*p - k_size) // s + 1
+    W_out = (W + 2*p - k_size) // s + 1
     return H_out, W_out
+
 
 def conv2d(image, kernel, stride=1, padding=0):
     """
     Apply 2D convolution to a single-channel image.
     """
-    image = np.asarray(image, dtype=float)
-    kernel = np.asarray(kernel, dtype=float)
+    image  = np.array(image, dtype=np.float32)
+    kernel = np.array(kernel, dtype=np.float32)   
     s = int(stride)
     p = int(padding)
 
-    H, W = image.shape[:2]
-    k_h, k_w = kernel.shape[:2]
+    H, W = image.shape
+    k_size = kernel.shape[0]
 
-    H_out, W_out = compute_ouput_dim(H, W, k_h, k_w, p, s)
+    H_out, W_out = compute_output_dim(H, W, p, s, k_size)
+    output = np.zeros((H_out, W_out), dtype=np.float32)
 
-    # padded image
     padded = np.pad(
         image, 
-        pad_width=padding, 
+        pad_width=p, 
         mode="constant", 
         constant_values=0
     )
 
-    output = np.zeros((H_out, W_out), dtype=float)
-
     for i in range(H_out):
         for j in range(W_out):
-            start_i = i*s
-            start_j = j*s
+            start_i = i * s
+            start_j = j * s
 
-            region = padded[start_i:start_i + k_h, start_j:start_j + k_w]
-            output[i, j] = np.sum(region * kernel)
-            
+            pad = padded[start_i: start_i + k_size, start_j: start_j + k_size]
+            output[i][j] = np.sum(pad * kernel)
+    
     return output.tolist()
