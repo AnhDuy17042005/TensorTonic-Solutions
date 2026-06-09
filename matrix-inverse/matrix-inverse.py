1ce7ac3ea95353d859782f10b296b91c3013504b
Nguyễn Võ Anh Duy <duy.nguyen.17042005@gmail.com>
2026-06-09 22:08:46 +0700

    Solve matrix-inverse

--- matrix-inverse/matrix-inverse.py
+++ matrix-inverse/matrix-inverse.py
@@ -5,13 +5,9 @@ def matrix_inverse(A):
     Returns: A_inv of shape (n, n) such that A @ A_inv ≈ I
     """
     A = np.asarray(A, dtype=float)
-    determinant = np.linalg.det(A)
+    det = np.linalg.det(A)
 
-    if A.ndim != 2 or A.shape[0] != A.shape[1]:
+    if A.shape[0] != A.shape[1] or det == 0:
         return None
     
-    if determinant == 0:
-        return None
-
     return np.linalg.inv(A)
-    
