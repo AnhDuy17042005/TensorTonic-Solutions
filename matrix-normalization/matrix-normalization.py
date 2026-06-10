7fb80cd2a4438dfc4f7b25e16c60985695f727db
Nguyễn Võ Anh Duy <duy.nguyen.17042005@gmail.com>
2026-06-10 11:51:15 +0700

    Solve matrix-normalization

--- matrix-normalization/matrix-normalization.py
+++ matrix-normalization/matrix-normalization.py
@@ -1,29 +1,26 @@
 import numpy as np
 
-def matrix_normalization(matrix, axis=None, norm_type='l2'):
+def matrix_normalization(matrix, axis=None, norm_type="l2"):
     """
     Normalize a 2D matrix along specified axis using specified norm.
-    """
-    den = 1
-    try:
-        matrix = np.asarray(matrix, dtype=float)
-    except:
-        None
+    """    
+    matrix = np.asarray(matrix, dtype=float)
 
     if matrix.ndim != 2:
         return None
 
     if norm_type not in ('l1', 'l2', 'max') or axis not in (0, 1, None):
         return None
-        
-    if norm_type == 'l1':
-        den = np.sum(abs(matrix), axis=axis, keepdims=True)
-    elif norm_type == 'l2':
-        den = np.sqrt(np.sum(matrix**2, axis=axis, keepdims=True))
-    elif norm_type == 'max':
-        den = np.max(abs(matrix), axis=axis, keepdims=True)
-
-    den = np.where(den == 0, 1, den)
     
-    return matrix/den
+    if norm_type == "l1":
+        denominator = np.sum(np.abs(matrix), axis=axis, keepdims=True)
         
+    elif norm_type == "l2":
+        denominator = np.sqrt(np.sum(matrix ** 2, axis=axis, keepdims=True))
+        
+    elif norm_type == "max":
+        denominator = np.max(np.abs(matrix), axis=axis, keepdims=True)
+
+    denominator = np.where(denominator == 0, 1, denominator)
+
+    return matrix / denominator
